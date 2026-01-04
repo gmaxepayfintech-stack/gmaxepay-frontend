0807c9dee696093dd89e206995bba7ae5f01b46e
madhanmsk <msmadhankumar2001@gmail.com>
2026-01-04 20:45:39 +0530

    Updated the code for the payout transaction

--- src/redux/reducer/payoutReducer.js
+++ src/redux/reducer/payoutReducer.js
@@ -1,10 +1,11 @@
-import { PAYOUT_BANK_LIST_SUCCESS } from "../actionType/payOutType";
+import { PAYOUT_BANK_LIST_SUCCESS, PAYOUT_TRANSACTION_SUCCESS } from "../actionType/payOutType";
 const initialState = {
     loading: false,
     error: null,
     payoutBankList: null,
     message: null,
     success: null,
+    payoutTransaction:null,
 };
 
 const payoutReducer = (state = initialState, action) => {
@@ -17,6 +18,14 @@ const payoutReducer = (state = initialState, action) => {
                 success: action?.payload?.status,
                 payoutBankList: action?.payload,
                 message: action?.payload?.message || action?.payload,
+            }
+        case PAYOUT_TRANSACTION_SUCCESS:
+            return{
+                ...state,
+                error:action?.payload?.error,
+                payoutTransaction: action?.payload,
+                status: action?.payload?.status,
+                message: action?.payload?.message,
             }
 
         default:
