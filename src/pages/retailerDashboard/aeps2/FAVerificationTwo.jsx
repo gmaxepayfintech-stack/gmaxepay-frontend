d961db91301f7b9fd641078d1924d4ff51438d64
madhanmsk <msmadhankumar2001@gmail.com>
2026-01-15 14:07:25 +0530

    Update the FA

--- src/pages/retailerDashboard/aeps2/FAVerificationTwo.jsx
+++ src/pages/retailerDashboard/aeps2/FAVerificationTwo.jsx
@@ -252,7 +252,7 @@ const FAVerificationTwo = () => {
 
     // Build proper XML structure without backslashes
     const pidOptions =
-      '<?xml version="1.0"?><PidOptions ver="1.0"><Opts env="P" fCount="1" fType="2" iCount="0" format="0" pidVer="2.0" timeout="15000" wadh="E0jzJ/P8UopUHAieZn8CKqS4WPMi5ZSYXgfnlfkWjrc=" posh="UNKNOWN" />' +
+      '<?xml version="1.0"?><PidOptions ver="1.0"><Opts env="P" fCount="1" fType="2" iCount="0" format="0" pidVer="2.0" timeout="15000" posh="UNKNOWN" />' +
       DString +
       custOpts +
       "</PidOptions>";
