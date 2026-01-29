59274b587f6b2640dc2330b37ca96a0e4055d821
madhanmsk <msmadhankumar2001@gmail.com>
2026-01-29 23:10:50 +0530

    Tax history updated

--- src/pages/DistributorDashboard/TaxHistory.jsx
+++ src/pages/DistributorDashboard/TaxHistory.jsx
@@ -224,7 +224,7 @@ const TaxHistory = () => {
                   onClick={() => {
                     if (card.title === "AEPS CW History") {
                       navigate(
-                        "/superDashboard/txn-history?view=aeps-cw-history",
+                        "/masterDistributerDashboard/tax-history?view=aeps-cw-history",
                       );
                     }
                   }}
