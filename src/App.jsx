b3e6bec606d92dab49d19bd2987e00f8b669b483
madhanmsk <msmadhankumar2001@gmail.com>
2026-01-29 16:21:49 +0530

    Error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -215,7 +215,7 @@ function App() {
 
   useEffect(() => {
     if (error && error !== prevErrorRef.current) {
-      // Only show notification if previous value was not null (i.e., value actually changed, not initial mount)
+console.log('error', error);
       if (prevErrorRef.current !== null) {
         showNotification({
           type: "error",
