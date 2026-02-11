af74b8f2c78d5afe3f1d98d6fa120bdf2196ddd0
madhanmsk <msmadhankumar2001@gmail.com>
2026-02-11 15:40:27 +0530

    Updated the reports

--- src/redux/reducer/reportsReducer.js
+++ src/redux/reducer/reportsReducer.js
@@ -1,4 +1,4 @@
-import { ADMIN_TXN_REPORT_SUCCESS, COMPANY_TXN_REPORT_SUCCESS } from "../actionType/reportsActionType";
+import { ADMIN_TXN_REPORT_SUCCESS, COMPANY_TXN_REPORT_SUCCESS, USER_TXN_REPORT_SUCCESS } from "../actionType/reportsActionType";
 
 const initialState = {
     loading: false,
@@ -8,6 +8,7 @@ const initialState = {
     success: null,
     adminTransaction: null,
     companyTransaction: null,
+    userTransaction: null,
 };
 
 const reportsReducer = (state = initialState, action) => {
@@ -28,7 +29,14 @@ const reportsReducer = (state = initialState, action) => {
                     status: action?.payload?.status,
                     message: action?.payload?.message,
                 }
-                
+                case USER_TXN_REPORT_SUCCESS:
+                    return{
+                        ...state,
+                        error: action?.payload?.error,
+                        userTransaction: action?.payload,
+                        status: action?.payload?.status,
+                        message: action?.payload?.message,
+                    }
         default:
             return state;
     }
