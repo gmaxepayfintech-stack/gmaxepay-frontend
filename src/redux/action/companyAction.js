d14b8cdd1e68deaa2230bb0a8bf1109b6826c0e0
madhanmsk <msmadhankumar2001@gmail.com>
2026-02-18 16:30:08 +0530

    Updated the code of setting in whitelabel

--- src/redux/action/companyAction.js
+++ src/redux/action/companyAction.js
@@ -9,3 +9,4 @@ export const clearCompany = () => ({
   type: CLEAR_COMPANY,
 });
 
+
--- src/redux/reducer/walletReducer.js
+++ src/redux/reducer/walletReducer.js
@@ -1,4 +1,4 @@
-import { WALLET_ALS_SUCCESS, WALLET_ALS_FAILURE, WALLET_BALANCE_SUCCESS, WALLET_BALANCE_FAILURE, COMPANY_WALLET_BALANCE_SUCCESS, COMPANY_WALLET_BALANCE_FAILURE, USER_WALLET_BALANCE_SUCCESS, USER_WALLET_BALANCE_FAILURE, EKYC_HUB_BALANCE_SUCCESS, EKYC_HUB_BALANCE_FAILURE, INSPAY_WALLET_BALANCE_SUCCESS, INSPAY_WALLET_BALANCE_FAILURE, BBPS_WALLET_BALANCE_SUCCESS, BBPS_WALLET_BALANCE_FAILURE, DASHBOARD_STATISTICS_SUCCESS, DASHBOARD_STATISTICS_FAILURE, USER_DASHBOARD_STATISTICS_SUCCESS, USER_DASHBOARD_STATISTICS_FAILURE } from "../actionType/walletActionType";
+import { WALLET_ALS_SUCCESS, WALLET_ALS_FAILURE, WALLET_BALANCE_SUCCESS, WALLET_BALANCE_FAILURE, COMPANY_WALLET_BALANCE_SUCCESS, COMPANY_WALLET_BALANCE_FAILURE, USER_WALLET_BALANCE_SUCCESS, USER_WALLET_BALANCE_FAILURE, EKYC_HUB_BALANCE_SUCCESS, EKYC_HUB_BALANCE_FAILURE, INSPAY_WALLET_BALANCE_SUCCESS, INSPAY_WALLET_BALANCE_FAILURE, BBPS_WALLET_BALANCE_SUCCESS, BBPS_WALLET_BALANCE_FAILURE, DASHBOARD_STATISTICS_SUCCESS, DASHBOARD_STATISTICS_FAILURE, USER_DASHBOARD_STATISTICS_SUCCESS, USER_DASHBOARD_STATISTICS_FAILURE, UPLOAD_FEVICON_SUCCESS } from "../actionType/walletActionType";
 
 const initialState = {
     loading: false,
@@ -23,6 +23,8 @@ const initialState = {
     userDashboardStatisticsError: null,
     success: null,
     message: null,
+    uploadFevicon: null,    
+    uploadFeviconError: null,
 };
 
 const walletReducer = (state = initialState, action) => {
@@ -224,6 +226,16 @@ const walletReducer = (state = initialState, action) => {
                 success: null,
                 message: action?.payload?.message || action?.payload,
             };
+        case UPLOAD_FEVICON_SUCCESS:
+            return{
+                ...state,
+                uploadFevicon: action?.payload,
+                uploadFeviconError: null,
+                loading: false,
+                error: null,
+                success: action?.payload?.status,
+                message: action?.payload?.message,
+            }
 
         default:
             return state;
