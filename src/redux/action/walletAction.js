52e35c1b562f0828ffb67eb60710a4dabc2313c1
madhanmsk <msmadhankumar2001@gmail.com>
2026-02-19 15:56:46 +0530

    Updated the code

--- src/redux/action/walletAction.js
+++ src/redux/action/walletAction.js
@@ -777,3 +777,56 @@ export const walletHistoryUsers = (payload) => async (dispatch) => {
         dispatch({ type: LOADING_END });
     }
 };
+
+export const surChargesHistory = (payload) => async (dispatch) => {
+    dispatch({ type: LOADING_START });
+    try {
+        const authToken = secureLocalStorage.getItem("userToken");
+        const response = await axios.post(
+            `${API_ROUTE}/api/v1/user/wallet/walletHistory`,
+            payload,
+            {
+                headers: {
+                    Authorization: `Bearer ${authToken}`,
+                },
+            }
+        );
+
+        const { data, status, message } = response?.data ?? {};
+        if (status === "SUCCESS") {
+            dispatch({
+                type: WALLET_HISTORY_USER_SUCCESS,
+                payload: { data, status, message },
+            });
+            return { data, status, message };
+        } else {
+            dispatch({
+                type: WALLET_HISTORY_USER_FAILURE,
+                payload: {
+                    status: response?.data?.status ?? "FAILURE",
+                    message: response?.data?.message ?? commonError,
+                },
+            });
+            return { status: response?.data?.status ?? "FAILURE", message: response?.data?.message ?? commonError };
+        }
+    } catch (error) {
+        if (isTokenExpiredError(error)) {
+            clearAllStorage();
+        }
+
+        const errorMessage = error.response ? error.response.data.message : error.message;
+        dispatch({
+            type: WALLET_HISTORY_USER_FAILURE,
+            payload: {
+                status: "FAILURE",
+                message: errorMessage,
+            },
+        });
+        return {
+            status: "FAILURE",
+            message: errorMessage,
+        };
+    } finally {
+        dispatch({ type: LOADING_END });
+    }
+};
