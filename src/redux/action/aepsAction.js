6adb3cf1499e31c75245f22e868f02f99a2e9553
madhanmsk <msmadhankumar2001@gmail.com>
2026-03-09 19:12:19 +0530

    Updated the code

--- src/redux/action/aepsAction.js
+++ src/redux/action/aepsAction.js
@@ -921,6 +921,57 @@ export const getAeps2TransactionDetails = (transactionId) => async (dispatch) =>
     }
 };
 
+export const getAepsTransactionDetailsCompany = (transactionId) => async (dispatch) => {
+    dispatch({ type: LOADING_START });
+    try {
+        const authToken = secureLocalStorage.getItem("userToken");
+
+        const response = await axios.post(
+            `${API_ROUTE}/api/v1/company/reports/aeps1/transactionDetails/${transactionId}`,
+            {},
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${authToken}`,
+                },
+            }
+        );
+
+        const { data: transactionDetailsCompany, status, message } = response?.data ?? {};
+        if (status === "SUCCESS") {
+            dispatch({
+                type: AEPS_TRANSACTION_DETAILS_COMPANY_SUCCESS,
+                payload: { data: transactionDetailsCompany, status, message },
+            });
+            return { data: transactionDetailsCompany, status, message };
+        } else {
+            dispatch({
+                type: AEPS_TRANSACTION_DETAILS_COMPANY_FAILURE,
+                payload: {
+                    status: response?.data?.status ?? "FAILURE",
+                    message: response?.data?.message ?? commonError,
+                },
+            });
+            return { status: response?.data?.status ?? "FAILURE", message: response?.data?.message ?? commonError };
+        }
+    } catch (error) {
+        const errorMessage = error.response ? error.response.data.message : error.message;
+        dispatch({
+            type: AEPS_TRANSACTION_DETAILS_COMPANY_FAILURE,
+            payload: {
+                status: "FAILURE",
+                message: errorMessage,
+            },
+        });
+        return {
+            status: "FAILURE",
+            message: errorMessage,
+        };
+    } finally {
+        dispatch({ type: LOADING_END });
+    }
+};
+
 export const getAeps2TransactionDetailsCompany = (transactionId) => async (dispatch) => {
     dispatch({ type: LOADING_START });
     try {
